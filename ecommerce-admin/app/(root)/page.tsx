748f3101c4e6b68bcb7be86159c59be80095b629
Siddhartha Tiwari <73224879+Sidontheway@users.noreply.github.com>
2023-12-06 09:28:52 +0000

    Second Commit

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,10 +1,22 @@
-import { Button } from "@/components/ui/button";
+"use client";
+import { useEffect } from "react";
 
-export default function Home() {
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+export default function SetupPage() {
+  const onOpen = useStoreModal((state)=> state.onOpen);
+  const isOpen = useStoreModal((state)=> state.isOpen);
+
+  useEffect(()=>{
+    if(!isOpen){
+      onOpen();
+    }
+  },[isOpen,onOpen]);
+
     return (
       <div className="p-4">
-            <Button size = "default" variant= "outline"> Click me</Button>    
-      </div>
+        Root Page
+       </div>
     )
   }
   
